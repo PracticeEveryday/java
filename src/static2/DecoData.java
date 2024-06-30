69338f59bbfc350be0521819f86eb86cb3034869
PracticeEveryday <developerforus@gmail.com>
2024-06-30 18:03:24 +0900

    feat: done static method

---
+++ src/static2/DecoData.java
@@ -0,0 +1,45 @@
+package static2;
+
+public class DecoData {
+    // 인스턴스 변수에 접근하기 위해서는 참조값을 무조건 알고 있어야 한다.
+    private int instanceValue;
+    private static int staticValue;
+
+    /**
+     * static 키워드가 붙은 정적 메서드는 정적 변수 또는 정적 메서드만 호출할 수 있다.
+     * Class 영역에 속한 메서드가 스택 영역이나, 힙 영역의 주소를 알 길이 없다. 참조값을 알길이 없다!!
+     */
+    public static void staticCall() {
+        instanceValue++; // 인스턴스 변수 접근 시 compile error
+        instanceMethod(); // 인스턴스 method 접근 시 compile error
+
+
+        staticValue++; // 정적 변수 접근 가능
+        staticMethod(); // 정적 메서드 접근 가능
+    }
+
+    public static void staticCall(DecoData data) {
+        // 참조 값을 매개변수로 넘겨주는 경우에는 해당 인스턴스의 인스턴스 변수, 메소드를 사용할 수 있다!
+        data.instanceValue++;
+        data.instanceMethod();
+    }
+
+    // 인스턴스 생성 후 메서드를 호출하는 것은 다 가능하다!
+    // 정적 변수 자기 자신의 메서드 모두 접근 가능!
+    public void instanceCall() {
+        instanceValue++; // 인스턴스 변수 접근 가능
+        instanceMethod(); // 인스턴스 method 가능
+
+
+        DecoData.staticValue++; // 정적 변수 접근 가능
+        staticMethod(); // 정적 메서드 접근 가능
+    }
+
+    public static void staticMethod() {
+        System.out.println("staticValue = " + staticValue);
+    }
+
+    public void instanceMethod() {
+        System.out.println("instanceValue = " + instanceValue);
+    }
+}
--- src/static2/DecoMain2.java
+++ src/static2/DecoMain2.java
@@ -1,6 +1,14 @@
 package static2;
 
 public class DecoMain2 {
+
+    /**
+     * main 함수는 다표적인 정적 메서드이다.
+     * 정적 메서드는 정적 메서드만 호출 할 수 있다.
+     * 엄연히 말하면 정적 메서드는 같은 클래스 내부의 정적 매서드만 호출 할 수 있다!
+     *
+     * 그렇기 때문에 main 함수에서 다른 함수를 호출하기 위해 static mehtod를 사용하곤 한 것이다!!
+     */
     public static void main(String[] args) {
         String s = "hello java";
 
